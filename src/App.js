8af031ec0ef57852cdac0f2adcc50e7fc0ec180a
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-21 20:40:38 -0300

    feat: Exemplo de useState: Utiliza um useState, passando um objeto dentro

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [state, setState] = useState({ count: 0, theme: "light" })
+
+  // function Increment(){
+  //   setCount((prevState) => prevState + 1);
+  // } 
 
   function Increment(){
-    setCount((prevState) => prevState + 1);
+    setState((prevState) => {
+      return {
+        ...prevState,
+        count: prevState.count + 1,
+        theme: 'dark',
+      }
+    });
   } 
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <button onClick={Increment}>Incremento</button>
     </div>
   );
