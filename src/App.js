3e2a158cb5d08d16dff42b97efb11a8f85190235
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-21 20:32:54 -0300

    feat: Exemplo de useState: Um contador que soma um número ao apertar o botão

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function Increment(){
+    setCount(count + 1);
+  } 
   return (
     <div>
-      
+      <h1>{count}</h1>
+      <button onClick={Increment}>Incremento</button>
     </div>
   );
 }
