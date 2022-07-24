343a4d022f86a8ee47253440f5b187849d0889e6
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-23 23:32:03 -0300

    feat: Cria pasta para exemplo de useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import { State } from '../src/hooks/State'
+import { State } from '../src/hooks/State';
+import { Effect } from '../src/hooks/Effect';
 
 function App() {
   return (
-    <State />
+    // <State />
+    <Effect />
   );
 }
 
---
+++ src/hooks/Effect/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Effect(){
+    return(
+        <div>
+            <h1>useEffect</h1>
+        </div>
+    );
+}
