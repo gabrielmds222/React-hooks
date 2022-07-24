a45815ad75363c5d2211b8db042d06cad7e828f2
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-24 07:37:39 -0300

    feat: Exemplo do useRef utilizando o .focus()

--- src/hooks/Ref/index.js
+++ src/hooks/Ref/index.js
@@ -3,17 +3,23 @@ import React, { useState,useEffect, useRef } from 'react';
 export function Ref(){
     const [name, setName] = useState('');
 
-    const renders = useRef(0);
+    const inputRef = useRef();
 
-    useEffect(() => {
-        renders.current = renders.current + 1;
-    })
+    function focusInput(){
+        inputRef.current.focus();
+    }
+
+    // const renders = useRef(0);
+
+    // useEffect(() => {
+    //     renders.current = renders.current + 1;
+    // });
 
     return(
         <div>
-            <input value={name} onChange={(e) => setName(e.target.value)}></input>
+            <input ref={inputRef} value={name} onChange={(e) => setName(e.target.value)}></input>
             <p>Hello, myname is {name}!!</p>
-            <p>Renders: {renders.current}</p>
+            <button onCLick={focusInput}>Focus input</button>
         </div>
     );
 }
