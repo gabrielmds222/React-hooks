9fb71729315d6eb59f3a33cc0eb9cba56bd69fd4
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-23 23:26:40 -0300

    feat: Refatora estrutura: Separa exemplo de useState em tela separada

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import { State } from '../src/hooks/State'
 
 function App() {
-  // const [count, setCount] = useState(0);
-  const [state, setState] = useState({ count: 0, theme: "light" })
-
-  // function Increment(){
-  //   setCount((prevState) => prevState + 1);
-  // } 
-
-  function Increment(){
-    setState((prevState) => {
-      return {
-        ...prevState,
-        count: prevState.count + 1,
-        theme: 'dark',
-      }
-    });
-  } 
   return (
-    <div>
-      <h1>{state.count}</h1>
-      <button onClick={Increment}>Incremento</button>
-    </div>
+    <State />
   );
 }
 
---
+++ src/hooks/State/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export function State() {
+  // const [count, setCount] = useState(0);
+  const [state, setState] = useState({ count: 0, theme: "light" })
+
+  // function Increment(){
+  //   setCount((prevState) => prevState + 1);
+  // } 
+
+  function Increment(){
+    setState((prevState) => {
+      return {
+        ...prevState,
+        count: prevState.count + 1,
+        theme: 'dark',
+      }
+    });
+  } 
+  return (
+    <div>
+      <h1>{state.count}</h1>
+      <button onClick={Increment}>Incremento</button>
+    </div>
+  );
+}
