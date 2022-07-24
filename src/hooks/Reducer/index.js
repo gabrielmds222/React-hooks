b399a233f13f71095ee85310f0424ecd5005e16e
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-24 08:36:29 -0300

    feat: Adiciona funcionalidade de marcar tarefa como completada usando o useReducer

--- src/hooks/Reducer/index.js
+++ src/hooks/Reducer/index.js
@@ -16,18 +16,25 @@ import React, { useState, useReducer } from 'react';
 // }
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case 'add-task':
-            return {
-                tasks: [
-                    ...state.tasks,
-                    { name: action.payload, isCompleted: false },
-                ],
-            };
-        default:
-            return state;
+    switch (action.type) {
+      case "add-task":
+        return {
+          ...state,
+          tasks: [...state.tasks, { name: action.payload, isCompleted: false }],
+        };
+      case "toggle-task":
+        return {
+          ...state,
+          tasks: state.tasks.map((item, index) =>
+            index === action.payload
+              ? { ...item, isCompleted: !item.isCompleted }
+              : item
+          ),
+        };
+      default:
+        return state;
     }
-}
+  };
 
 export function Reducer(){
     const [state, dispatch] = useReducer(reducer, { tasks: [] });
@@ -35,12 +42,27 @@ export function Reducer(){
 
     return(
         <div>
-            <input value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
-            <button onClick={() => dispatch({ type: 'add-task', payload: inputValue })}>Adicionar</button>
+            <input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button
+            onClick={() => {
+            dispatch({ type: "add-task", payload: inputValue });
+            setInputValue("");
+            }}
+        >
+            Adicionar
+        </button>
 
-            {state.tasks.map((task) => (
-                <h1>{task.name}</h1>
-            ))}
-        </div>
-    );
-}
+        {state.tasks.map((task, index) => (
+            <p
+            onClick={() => dispatch({ type: "toggle-task", payload: index })}
+            style={{ textDecoration: task.isCompleted ? "line-through" : "none" }}
+            >
+            {task.name}
+            </p>
+        ))}
+    </div>
+  );
+};
