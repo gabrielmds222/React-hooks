e6bf5b04e9251d4641c46b367e386a9e5fc94461
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-24 01:00:50 -0300

    feat: Cria arquivo de estudo do hook useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 // import { State } from '../src/hooks/State';
-import { Effect } from '../src/hooks/Effect';
+// import { Effect } from '../src/hooks/Effect';
+import { Ref } from '../src/hooks/Ref';
 
 function App() {
   return (
     // <State />
-    <Effect />
+    <Ref />
   );
 }
 
---
+++ src/hooks/Ref/index.js
@@ -0,0 +1,9 @@
+import React, { useState, useRef } from 'react';
+
+export function Ref(){
+    return(
+        <div>
+            <h1>useRef exemplo</h1>
+        </div>
+    );
+}
