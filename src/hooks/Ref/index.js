081f3033af85dd01fc21311368a3d62abaa57d95
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-24 07:32:19 -0300

    feat: Primeiro exemplo de usabilidade do useRef

--- src/hooks/Ref/index.js
+++ src/hooks/Ref/index.js
@@ -1,9 +1,19 @@
-import React, { useState, useRef } from 'react';
+import React, { useState,useEffect, useRef } from 'react';
 
 export function Ref(){
+    const [name, setName] = useState('');
+
+    const renders = useRef(0);
+
+    useEffect(() => {
+        renders.current = renders.current + 1;
+    })
+
     return(
         <div>
-            <h1>useRef exemplo</h1>
+            <input value={name} onChange={(e) => setName(e.target.value)}></input>
+            <p>Hello, myname is {name}!!</p>
+            <p>Renders: {renders.current}</p>
         </div>
     );
 }
