9929207f8d795982959096636e5bc23f9e4901a8
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-25 23:56:58 -0300

    feat: Utilza exemplo de função com problema de performance

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import React from 'react';
 // import { Effect } from '../src/hooks/Effect';
 // import { Ref } from '../src/hooks/Ref';
 // import { Reducer } from '../src/hooks/Reducer';
-import ThemeContextProvider from '../src/hooks/Context';
+// import ThemeContextProvider from '../src/hooks/Context';
+import { Memo } from '../src/hooks/Memo';
+
 
 import Message from './Message';
 import Greeting from './Greeting';
 
 function App() {
   return (
-    <ThemeContextProvider>
-      <Message />
-      <Greeting />
-    </ThemeContextProvider>
+    <Memo />
   );
 }
 
--- src/hooks/Memo/index.js
+++ src/hooks/Memo/index.js
@@ -1,1 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+export function Memo(){
+    const [number, setNumber] = useState(1);
+    const [text, setText] = useState('');
+
+    const slowFuntion = (num) => {
+        console.log('função foi chamada');
+        for(let i = 0; i <= 10000; i++){}
+        return num * 2;
+    }
+
+    const doubleNumber = slowFuntion(number);
+
+    return(
+        <>
+            <p>{number}</p>
+            <input value={text} onChange={(e) => setText(e.target.value)} />
+            <button onClick={() => setNumber(2)}>Increment</button>
+            <p>text: {text}</p>
+        </>
+    );
+}
